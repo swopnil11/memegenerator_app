675d6cf881004401d0f624e890373a29bd047fbb
Swopnil Parajuli <swopnilparajuli01@gmail.com>
2023-12-14 23:26:00 +0545

    Update

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css';
+
+export default function NavBar() {
+  return (
+    <header className="navbar">
+        <img src="images/troll.png" alt="trollface" className="navbar--image"/>
+        <h2 className="navbar--header">Meme Generator</h2>
+    </header>
+  )
+}
